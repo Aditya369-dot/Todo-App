956653da5c9b3428b085c02cdff3c209034d1047
aditya <adityabholla@gmail.com>
2024-02-01 13:12:43 -0800

    Implimented the Add button

--- GUI.py
+++ GUI.py
@@ -2,10 +2,27 @@ import Function
 import PySimpleGUI as sg
 
 label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter a Todo")
+input_box = sg.InputText(tooltip="Enter a Todo", key='todo')
 add_button = sg.Button("Add")
 
 
-window = sg.Window(' My Trajectory App', layout=[[label], [input_box, add_button]])
-window.read()
+window = sg.Window(' My Trajectory App',
+                   layout=[[label], [input_box, add_button]],
+                   font=('Helvetica', 20))
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos = Function.get_todos()
+            new_todo = values['todo'] + "\n"
+            todos.append(new_todo)
+            Function.write_todos(todos)
+        case sg.WIN_CLOSED:
+            break
+
+
+
 window.close()
